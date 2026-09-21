5e67b562a55be5d8b6d46e4b5d11e8057de2d4f5
Veronika <675269@yaya>
2026-09-21 13:14:48 +0300

    update

--- Task3.Unique/Program.cs
+++ Task3.Unique/Program.cs
@@ -1,11 +1,51 @@
 using System;
-namespace Task3
+
+class Program
 {
-   class Program
-   {
-     static void Main()
-     {
-       Console.WriteLine("Hello, World!");
-     }
-   }
+    static void Main()
+    {
+        int[] source = { 1, 2, 2, 3, 4, 4, 4, 5 };
+
+        Console.WriteLine("Исходный:  " + string.Join(", ", source));
+
+        int[] unique = GetUnique(source);
+
+        Console.WriteLine("Уникальные: " + string.Join(", ", unique));
+    }
+
+    public static int[] GetUnique(int[] source)
+    {
+        int[] temp = new int[source.Length];
+        
+        int uniqueCount = 0;
+
+        foreach (int item in source)
+        {
+            bool isAlreadyExist = false; 
+
+            for (int i = 0; i < uniqueCount; i++)
+            {
+                if (temp[i] == item)
+                {
+                    isAlreadyExist = true; 
+                    break;                 
+                }
+            }
+
+            if (!isAlreadyExist)
+            {
+                temp[uniqueCount] = item;
+                uniqueCount++; 
+            }
+        }
+
+        int[] result = new int[uniqueCount];
+
+        for (int i = 0; i < uniqueCount; i++)
+        {
+            result[i] = temp[i];
+        }
+
+        return result;
+    }
 }
